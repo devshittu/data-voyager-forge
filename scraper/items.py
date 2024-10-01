ff3d95830a419d3e7289f234db6edba24930be78
Muhammed Shittu <devshittu@gmail.com>
2024-10-01 07:26:01 +0100

    Add items

--- scraper/items.py
+++ scraper/items.py
@@ -3,10 +3,15 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
+
 import scrapy
 
 
 class ScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    content = scrapy.Field()
+    images = scrapy.Field()
+    videos = scrapy.Field()
+    date = scrapy.Field()
+    url = scrapy.Field()
+    metadata = scrapy.Field()
